477bcbee7b5af7b020a8c849dd5452b6066b4a37
dogtwelve <x3191@@hotmail.com>
2017-11-14 09:41:21 +0800

    NSStringFromClass([AppController class]

--- sources/skycrasher/launcher/iOS/main.m
+++ sources/skycrasher/launcher/iOS/main.m
@@ -1,13 +1,19 @@
 #import <UIKit/UIKit.h>
-
+#import "Delegate.h"
 
 int main(int argc, char *argv[])
 {
-	NSAutoreleasePool*		pool = [NSAutoreleasePool new];
-	
-	UIApplicationMain(argc, argv, nil, @"AppController");
-	
-	[pool release];
-	
-	return 0;
+//	NSAutoreleasePool*		pool = [NSAutoreleasePool new];
+//	
+//	UIApplicationMain(argc, argv, nil, @"AppController");
+//	[pool release];
+//	
+//	return 0;
+    
+    @autoreleasepool {
+        return UIApplicationMain(argc,
+                                 argv,
+                                 nil,
+                                 NSStringFromClass([AppController class]));
+    }
 }
